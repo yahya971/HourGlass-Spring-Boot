eba817684d6f0bd5c4efcff518b40119769847e1
Yahya Derbeli <derbeliy97@gmail.com>
2019-10-10 19:36:59 +0200

    Initialisation

---
+++ Hourglass/src/main/java/com/Project/Hourglass/HourglassApplication.java
@@ -0,0 +1,13 @@
+package com.Project.Hourglass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourglassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourglassApplication.class, args);
+	}
+
+}
