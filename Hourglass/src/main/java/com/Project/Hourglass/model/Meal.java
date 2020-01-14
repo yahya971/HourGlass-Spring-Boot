db920b3dc0914eef8e8bb5429d3793c58b4a4908
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2020-01-14 14:48:06 +0100

    nutitional analysis API

--- Hourglass/src/main/java/com/Project/Hourglass/model/Meal.java
+++ Hourglass/src/main/java/com/Project/Hourglass/model/Meal.java
@@ -17,25 +17,6 @@ import java.util.Set;
 public class Meal {
 
 
-    public Meal(long id,String name, @NotNull LocalTime time, String type, String description, float caloricValue, String photo,
-			@NotNull boolean isTaken, String recipe, String ingredients, Coach coach,
-			Set<Nutritionalprogram> nutritionalPrograms,Long preparationTime) {
-		super();
-		this.id = id;
-		this.time = time;
-		this.type = type;
-		this.description = description;
-		this.caloricValue = caloricValue;
-		this.photo = photo;
-		this.isTaken = isTaken;
-		this.recipe = recipe;
-		this.ingredients = ingredients;
-		this.coach = coach;
-		this.nutritionalPrograms = nutritionalPrograms;
-		this.name=name;
-		this.preparationTime=preparationTime;
-	}
-
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="meal_id")
@@ -49,6 +30,48 @@ public class Meal {
     private String description;
     
     private float caloricValue;
+
+    private String carbKcal;
+
+    private String energKcal;
+
+    private String fatKcal;
+
+    private String protKcal;
+
+    private String totalWeight;
+
+    private String dietLabels;
+
+    private String healthLabels;
+
+    private String calcium;
+
+    private String carbs;
+
+    private String cholesterol;
+
+    private String fat;
+
+    private String potassium;
+
+    private String magnesium;
+
+    private String protein;
+
+    private String sugar;
+
+    private String vitE;
+
+    private String vitA;
+
+    private String vitB6;
+
+    private String vitD;
+
+    private String vitK;
+
+    private String water;
 
     @Lob
     private String photo;
@@ -83,7 +106,48 @@ public class Meal {
     public Meal() {
     }
 
-  
+    public Meal(String name, @NotNull LocalTime time, String type, String description, float caloricValue,
+                String carbKacl, String energKcal, String fatKcal, String protKcal, String totalWeight,
+                String dietLabels, String healthLabels, String calcium, String carbs, String cholesterol,
+                String fat, String potassium, String magnesium, String protein, String sugar, String vitE,
+                String vitA, String vitB6, String vitD, String vitK, String water, String photo,
+                @NotNull boolean isTaken, String recipe, String ingredients, Coach coach, Long preparationTime,
+                Set<Nutritionalprogram> nutritionalPrograms) {
+        this.name = name;
+        this.time = time;
+        this.type = type;
+        this.description = description;
+        this.caloricValue = caloricValue;
+        this.carbKacl = carbKacl;
+        this.energKcal = energKcal;
+        this.fatKcal = fatKcal;
+        this.protKcal = protKcal;
+        this.totalWeight = totalWeight;
+        this.dietLabels = dietLabels;
+        this.healthLabels = healthLabels;
+        this.calcium = calcium;
+        this.carbs = carbs;
+        this.cholesterol = cholesterol;
+        this.fat = fat;
+        this.potassium = potassium;
+        this.magnesium = magnesium;
+        this.protein = protein;
+        this.sugar = sugar;
+        this.vitE = vitE;
+        this.vitA = vitA;
+        this.vitB6 = vitB6;
+        this.vitD = vitD;
+        this.vitK = vitK;
+        this.water = water;
+        this.photo = photo;
+        this.isTaken = isTaken;
+        this.recipe = recipe;
+        this.ingredients = ingredients;
+        this.coach = coach;
+        this.preparationTime = preparationTime;
+        this.nutritionalPrograms = nutritionalPrograms;
+    }
+
     public long getId() {
         return id;
     }
@@ -106,6 +170,174 @@ public class Meal {
 
     public void setCaloricValue(float caloricValue) {
         this.caloricValue = caloricValue;
+    }
+
+    public String getCarbKacl() {
+        return carbKacl;
+    }
+
+    public void setCarbKacl(String carbKacl) {
+        this.carbKacl = carbKacl;
+    }
+
+    public String getEnergKcal() {
+        return energKcal;
+    }
+
+    public void setEnergKcal(String energKcal) {
+        this.energKcal = energKcal;
+    }
+
+    public String getFatKcal() {
+        return fatKcal;
+    }
+
+    public void setFatKcal(String fatKcal) {
+        this.fatKcal = fatKcal;
+    }
+
+    public String getProtKcal() {
+        return protKcal;
+    }
+
+    public void setProtKcal(String protKcal) {
+        this.protKcal = protKcal;
+    }
+
+    public String getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(String totalWeight) {
+        this.totalWeight = totalWeight;
+    }
+
+    public String getDietLabels() {
+        return dietLabels;
+    }
+
+    public void setDietLabels(String dietLabels) {
+        this.dietLabels = dietLabels;
+    }
+
+    public String getHealthLabels() {
+        return healthLabels;
+    }
+
+    public void setHealthLabels(String healthLabels) {
+        this.healthLabels = healthLabels;
+    }
+
+    public String getCalcium() {
+        return calcium;
+    }
+
+    public void setCalcium(String calcium) {
+        this.calcium = calcium;
+    }
+
+    public String getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(String carbs) {
+        this.carbs = carbs;
+    }
+
+    public String getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(String cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public String getFat() {
+        return fat;
+    }
+
+    public void setFat(String fat) {
+        this.fat = fat;
+    }
+
+    public String getPotassium() {
+        return potassium;
+    }
+
+    public void setPotassium(String potassium) {
+        this.potassium = potassium;
+    }
+
+    public String getMagnesium() {
+        return magnesium;
+    }
+
+    public void setMagnesium(String magnesium) {
+        this.magnesium = magnesium;
+    }
+
+    public String getProtein() {
+        return protein;
+    }
+
+    public void setProtein(String protein) {
+        this.protein = protein;
+    }
+
+    public String getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(String sugar) {
+        this.sugar = sugar;
+    }
+
+    public String getVitE() {
+        return vitE;
+    }
+
+    public void setVitE(String vitE) {
+        this.vitE = vitE;
+    }
+
+    public String getVitA() {
+        return vitA;
+    }
+
+    public void setVitA(String vitA) {
+        this.vitA = vitA;
+    }
+
+    public String getVitB6() {
+        return vitB6;
+    }
+
+    public void setVitB6(String vitB6) {
+        this.vitB6 = vitB6;
+    }
+
+    public String getVitD() {
+        return vitD;
+    }
+
+    public void setVitD(String vitD) {
+        this.vitD = vitD;
+    }
+
+    public String getVitK() {
+        return vitK;
+    }
+
+    public void setVitK(String vitK) {
+        this.vitK = vitK;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
     }
 
     public String getPhoto() {
